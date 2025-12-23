51bdf540756e3a7f85c7b2879d37924fe6de40ab
w1l238 <wdooley15@outlook.com>
2025-12-23 15:40:31 -0500

    Integrate app background variable.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,18 @@ import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import Results from './pages/Results';
 import Settings from './pages/Settings';
+import Library from './pages/Library';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const savedBg = localStorage.getItem('app_background');
+    if (savedBg) {
+      document.documentElement.style.setProperty('--app-background', savedBg);
+    }
+  }, []);
+
   return (
     <>
       <NavBar />
@@ -13,6 +22,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/results" element={<Results />} />
+          <Route path="/library" element={<Library />} />
           <Route path="/settings" element={<Settings />} />
         </Routes>
       </main>
